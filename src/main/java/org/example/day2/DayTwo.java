6baf3880d205d0bce6dd093f3e4def8bd14a4bf5
abdul sule <suleabdul378@yahoo.com>
2023-10-10 11:33:43 +0100

    first commit

---
+++ src/main/java/org/example/day2/DayTwo.java
@@ -0,0 +1,17 @@
+package org.example.day2;
+
+import org.example.library.Book;
+
+public class DayTwo {
+    public static void main(String[] args) {
+        String hello = new String("Hello");
+        System.out.println("This is the first Hello " + hello);
+        System.out.println(new String("Abdul"));
+
+        Book libraryBook = new Book();
+
+        libraryBook.setThenameforme("SAB IS A SAGE");
+    }
+
+
+}
